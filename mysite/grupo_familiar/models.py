d93d6f2cf664cc93bd8313ca36501b0a63d3fac4
Pedro Ricardo <rpedroricardo87@gmail.com>
2025-11-20 16:27:21 -0300

    finalizando modelo familia

--- mysite/grupo_familiar/models.py
+++ mysite/grupo_familiar/models.py
@@ -1,19 +1,14 @@
 from django.db import models
-from usuario.models import Pessoa
+from usuario.models import Usuario
 # Create your models here.
 class Familia(models.Model):
     nome = models.CharField(max_length=100, blank=False, null=False)
 
     @property
     def membros(self):
-        return MembroFamilia.objects.filter(familia=self).exclude(pessoa__nome='AdminFamilia')
+        return Usuario.objects.filter(familia=self).exclude(pessoa__papel='adminFamilia')
     @property
-    def chefe(self):
-        return MembroFamilia.objects.filter(familia=self, pessoa__nome='AdminFamilia')
+    def chefes(self):
+        return Usuario.objects.filter(familia=self, pessoa__papel='adminFamilia')
     def __str__(self):
         return f"{self.nome}"
-class MembroFamilia(models.Model):
-    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE, related_name='usuario_familia', blank=False, null=False)
-    familia = models.ForeignKey(Familia, on_delete=models.CASCADE, related_name='familia', blank=False, null=False)
-    def __str__(self):
-        return f"{self.pessoa.nome} - {'Chefe' if self.pessoa.papel == 'AdminFamilia' else 'Membro'} da Família {self.familia.nome}"
