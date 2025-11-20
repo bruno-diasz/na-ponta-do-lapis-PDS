1c0bf88180e36b02339b79283524418e433b52ad
Eduardo Medeiros <eduardo.aguiar@escolar.ifrn.edu.br>
2025-11-20 20:10:54 -0300

    corrigindo imports

--- mysite/contas/models.py
+++ mysite/contas/models.py
@@ -1,14 +1,14 @@
-from django.db.models import Model, CharField, FloatField, ForeignKey
+from django.db import models
 from usuario.models import Usuario
 
 # Create your models here.
 
-class ContaFinanceira(Model):
+class ContaFinanceira(models.Model):
     TIPOS_CONTA = (('CREDITO', 'Crédito'), ('CREDITO/DEBITO', 'Crédito/Débito'), ('DEBITO', 'Débito'))
-    nome = CharField('Nome da Conta', max_length=100, blank=False, null=False)
-    saldo = FloatField('Saldo da Conta', default=0, blank=False, null=False)
-    tipo = CharField('Tipo da Conta', max_length=14, choices=TIPOS_CONTA, default='CREDITO', blank=False, null=False)
-    id_usuario = ForeignKey(Usuario, "Usuário da Conta", max_length=254, blank=False, null=False, related_name="contas_financeiras")
+    nome = models.CharField(max_length=100, blank=False, null=False, verbose_name='Nome da Conta')
+    saldo = models.FloatField(default=0, blank=False, null=False, verbose_name='Saldo da Conta')
+    tipo = models.CharField(max_length=14, choices=TIPOS_CONTA, default='CREDITO', blank=False, null=False, verbose_name='Tipo da Conta')
+    id_usuario = models.ForeignKey(Usuario, max_length=254, on_delete=models.CASCADE, blank=False, null=False, related_name="contas_financeiras", verbose_name="Usuário da Conta")
 
     def __str__(self):
         return f"{self.nome} - {self.saldo} - {self.tipo}"
