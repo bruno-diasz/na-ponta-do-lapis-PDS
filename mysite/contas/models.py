d1d82c53ff40664d064ceda413120d82d445f505
Eduardo Medeiros <eduardo.aguiar@escolar.ifrn.edu.br>
2025-11-29 11:15:45 -0300

    adicionando o método de validação de modelo e modificando os imports

--- mysite/contas/models.py
+++ mysite/contas/models.py
@@ -1,14 +1,43 @@
-from django.db import models
+from django.db.models import Model, CharField, FloatField, ForeignKey, CASCADE
+from django.core.exceptions import ValidationError
 from usuario.models import Usuario
 
 # Create your models here.
 
-class ContaFinanceira(models.Model):
+class ContaFinanceira(Model):
     TIPOS_CONTA = (('CREDITO', 'Crédito'), ('CREDITO/DEBITO', 'Crédito/Débito'), ('DEBITO', 'Débito'))
-    nome = models.CharField(max_length=100, blank=False, null=False, verbose_name='Nome da Conta')
-    saldo = models.FloatField(default=0, blank=False, null=False, verbose_name='Saldo da Conta')
-    tipo = models.CharField(max_length=14, choices=TIPOS_CONTA, default='CREDITO', blank=False, null=False, verbose_name='Tipo da Conta')
-    id_usuario = models.ForeignKey(Usuario, max_length=254, on_delete=models.CASCADE, blank=False, null=False, related_name="contas_financeiras", verbose_name="Usuário da Conta")
+    nome = CharField(max_length=100, blank=False, null=False, verbose_name='Nome da Conta')
+    saldo = FloatField(default=0, blank=False, null=False, verbose_name='Saldo da Conta')
+    tipo = CharField(max_length=14, choices=TIPOS_CONTA, default='', blank=False, null=False, verbose_name='Tipo da Conta')
+    id_usuario = ForeignKey(Usuario, on_delete=CASCADE, blank=False, null=False, related_name="contas_financeiras", verbose_name='Usuário da Conta')
+
+    class Meta:
+        verbose_name = 'Conta Financeira'
+        verbose_name_plural = 'Contas Financeiras'
+
+    def clean(self):
+        erros = {}
+
+        try:
+            super().clean()
+
+        except ValidationError as e:
+            erros.update(e.message_dict)
+
+        if len(self.nome) < 3:
+            erros['nome'] = 'O nome da conta deve ter pelo menos 3 caracteres.'
+        
+        if not self.saldo:
+            erros['saldo'] = 'O saldo da conta é obrigatório.'
+        
+        if self.tipo not in self.tipo.choices:
+            erros['tipo'] = 'O tipo da conta deve ser Crédito, Crédito e Débito ou Débito'
+        
+        if not self.tipo:
+            erros['tipo'] = 'O tipo da conta é obrigatório.'
+        
+        if erros:
+            raise ValidationError(erros)
 
     def set_nome(self, novo_nome):
         if (not isinstance(novo_nome, str) or novo_nome == ""):
