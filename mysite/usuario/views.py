763f6d9425e498602db344ec02ac14eb4f7fe291
Lucas H B Medeiros <120293165+Lucaasshq@users.noreply.github.com>
2025-12-02 01:18:00 +0000

    feat: iniciando auth

--- mysite/usuario/views.py
+++ mysite/usuario/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.test import TestCase
+from django.http import HttpResponse
+
 
 # Create your tests here.
 
@@ -7,4 +9,12 @@ def login(request):
     return render(request, "login.html")
 
 def cadastro(request):
-    return render(request, "cadastro.html")
+    if request.method == "GET":
+        return render(request, "cadastro.html")
+    else:
+        email = request.POST.get("email")
+        nome = request.POST.get("nome")
+        senha = request.POST.get("senha")
+        confirma_senha = request.POST.get("confirma_senha")
+        print(email, nome, senha, confirma_senha)
+        return HttpResponse("Dados recebidos com sucesso!")
