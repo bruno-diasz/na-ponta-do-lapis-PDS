be677e320d4caf997a9ffef40dcad261b0ad4bb0
Bruno Dias <brunoipb1@gmail.com>
2026-04-11 16:52:07 -0300

    feat: implementa metas

--- npl-back-end/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/entity/Meta.java
+++ npl-back-end/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/entity/Meta.java
@@ -1,11 +1,19 @@
 package com.npl.na_ponta_do_lapis.entity;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.persistence.*;
-import org.hibernate.annotations.CreationTimestamp;
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "meta")
