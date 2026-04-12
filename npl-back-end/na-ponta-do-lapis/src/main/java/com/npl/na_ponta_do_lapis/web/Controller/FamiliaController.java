508f336f6fcc808e0a57d21b9db5cec0f4d8d038
Pedro <rpedroricardo87@gmail.com>
2026-04-12 17:23:05 -0300

    feat: criando controller da familia

---
+++ npl-back-end/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/web/Controller/FamiliaController.java
@@ -0,0 +1,55 @@
+package com.npl.na_ponta_do_lapis.web.Controller;
+
+
+import com.npl.na_ponta_do_lapis.service.FamiliaService;
+import com.npl.na_ponta_do_lapis.web.Controller.dto.FamiliaDTO;
+import com.npl.na_ponta_do_lapis.web.Controller.dto.FamiliaResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/familia")
+@Tag(name = "Família", description = "Gerenciamento de Famílias")
+public class FamiliaController {
+
+    private FamiliaService familiaService;
+
+    public FamiliaController(FamiliaService familiaService){ this.familiaService = familiaService;}
+
+    @Operation(summary = "Listar Famílias")
+    @GetMapping
+    private ResponseEntity<List<FamiliaResponseDTO>> listarFamilias(){
+        return ResponseEntity.status(HttpStatus.OK).body(familiaService.listarFamilias());
+    }
+
+    @Operation(summary = "Buscar por Id")
+    @GetMapping("/{id}")
+    private ResponseEntity<FamiliaResponseDTO> buscarPorId(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(familiaService.buscarFamiliaPorID(id));
+    }
+
+    @Operation(summary = "Cadastrar Família")
+    @PostMapping
+    private ResponseEntity<FamiliaResponseDTO> cadastrarFamilia(FamiliaDTO usuarioDTO){
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(familiaService.criarFamilia(usuarioDTO));
+    }
+
+    @Operation(summary = "Excluir Família")
+    @DeleteMapping("/{id}")
+    private ResponseEntity<FamiliaResponseDTO> excluirFamilia(@PathVariable Long id){
+        familiaService.excluirFamilia(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @Operation(summary = "Editar Família")
+    @PutMapping("/{id}")
+    private ResponseEntity<FamiliaResponseDTO> editarFamilia(@PathVariable Long id, String nome, String foto){
+        return ResponseEntity.status(HttpStatus.OK).body(familiaService.editarFamilia(id, new FamiliaDTO(nome, foto)));
+    }
+
+}
