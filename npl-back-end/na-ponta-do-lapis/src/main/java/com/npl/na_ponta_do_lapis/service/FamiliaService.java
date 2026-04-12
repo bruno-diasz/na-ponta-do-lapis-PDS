4b3476d67f0f3335559ee2d3e85a4f73cf579658
Pedro <rpedroricardo87@gmail.com>
2026-04-12 17:22:45 -0300

    feat: criando service da familia

---
+++ npl-back-end/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/service/FamiliaService.java
@@ -0,0 +1,72 @@
+package com.npl.na_ponta_do_lapis.service;
+
+import com.npl.na_ponta_do_lapis.entity.Familia;
+import com.npl.na_ponta_do_lapis.repository.FamiliaRepository;
+import com.npl.na_ponta_do_lapis.web.Controller.dto.FamiliaDTO;
+import com.npl.na_ponta_do_lapis.web.Controller.dto.FamiliaResponseDTO;
+import com.npl.na_ponta_do_lapis.web.Controller.dto.UsuarioResponseDTO;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FamiliaService {
+    private final FamiliaRepository familiaRepository;
+
+    public FamiliaService( FamiliaRepository familiaRepository){this.familiaRepository = familiaRepository;}
+
+    @Transactional
+    public FamiliaResponseDTO criarFamilia(FamiliaDTO familiaDTO) {
+        Familia familia = familiaDTO.toEntity();
+        familiaRepository.save(familia);
+        return new FamiliaResponseDTO(familia);
+    }
+
+    @Transactional
+    public void excluirFamilia(long id){
+        familiaRepository.deleteById(id);
+    }
+
+    @Transactional
+    public FamiliaResponseDTO editarFamilia(long id, FamiliaDTO familiaDTO){
+        Familia familia = familiaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Família de ID " + id + " não existe!"));
+        if (familiaDTO.nome() != null){
+            if (familiaDTO.nome().isBlank()) {
+                throw new RuntimeException("Nome da família não pode ser vazio.");
+            }
+            familia.setNome(familiaDTO.nome());
+        }
+        if (familiaDTO.foto() != null){
+            familia.setFotoFamilia(familiaDTO.foto());
+        }
+        familiaRepository.save(familia);
+        return new FamiliaResponseDTO(familia);
+    }
+
+    //@Transactional
+    //public UsuarioResponseDTO adicionarUsuarioNaFamilia(){}
+
+    //@Transactional
+    //public UsuarioResponseDTO removerUsuarioDaFamilia(){}
+
+    //public List<UsuarioResponseDTO> listarUsuariosDaFamilia(){}
+
+    //@Transactional
+    //public UsuarioResponseDTO tornarAdminFamilia(){}
+
+    public List<FamiliaResponseDTO> listarFamilias(){
+        return familiaRepository.findAll()
+                .stream()
+                .map(familia -> new FamiliaResponseDTO((Familia) familia))
+                .toList();
+    }
+
+    public FamiliaResponseDTO buscarFamiliaPorID(long id){
+        return familiaRepository.findById(id)
+                .map(familia -> new FamiliaResponseDTO((Familia) familia))
+                .orElseThrow(() -> new RuntimeException("Família de ID " + id + " não existe!"));
+    }
+
+}
