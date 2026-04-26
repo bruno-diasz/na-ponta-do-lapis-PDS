68985123e0c7343dcb81fc6623dc079dca93ccd6
Lucas H B Medeiros <120293165+Lucaasshq@users.noreply.github.com>
2026-04-26 19:56:50 -0300

    Refactor TransacaoRequestDTO to resolve merge conflicts

--- backend/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/web/dto/TransacaoRequestDTO.java
+++ backend/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/web/dto/TransacaoRequestDTO.java
@@ -1,13 +1,7 @@
 package com.npl.na_ponta_do_lapis.web.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
-<<<<<<< HEAD
-<<<<<<< HEAD
 import com.npl.na_ponta_do_lapis.entity.enums.EstadoTransacao;
-=======
->>>>>>> e4f88b5 (refactor:corrigindo POST criaTransacoes e implementando regra de negorcio ao criar transacao)
-=======
 import com.npl.na_ponta_do_lapis.entity.enums.EstadoTransacao;
->>>>>>> f2c067a (refactor(Transacoes ): corrigindo endpoint transacao, add regra de negocio no atualizar Transacao e melhorando resposta dos endpoits transacoes)
 import com.npl.na_ponta_do_lapis.entity.enums.TipoTransacao;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
@@ -29,23 +23,15 @@ public record TransacaoRequestDTO(
 
         @NotNull(message = "O Tipo transacao não pode ser null!")
         TipoTransacao tipo,
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> f2c067a (refactor(Transacoes ): corrigindo endpoint transacao, add regra de negocio no atualizar Transacao e melhorando resposta dos endpoits transacoes)
 
         EstadoTransacao estado,
 
         @NotNull(message = "ID categoria não pode ser null!")
         Long idCategoria,
 
-<<<<<<< HEAD
-=======
         @NotNull(message = "ID categoria não pode ser null!")
         Long idCategoria,
->>>>>>> e4f88b5 (refactor:corrigindo POST criaTransacoes e implementando regra de negorcio ao criar transacao)
-=======
->>>>>>> f2c067a (refactor(Transacoes ): corrigindo endpoint transacao, add regra de negocio no atualizar Transacao e melhorando resposta dos endpoits transacoes)
+
         @NotNull(message = "ID conta financeira não pode ser null")
         Long idContaFinanceira,
 
@@ -53,13 +39,7 @@ public record TransacaoRequestDTO(
         @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
         LocalDateTime dataHora
 ) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
     public TransacoesResponseDTO toTransacoesResponseDTO(){
         return new TransacoesResponseDTO(descricao, valor, tipo, idCategoria, idContaFinanceira, dataHora);
     }
->>>>>>> e4f88b5 (refactor:corrigindo POST criaTransacoes e implementando regra de negorcio ao criar transacao)
-=======
->>>>>>> f2c067a (refactor(Transacoes ): corrigindo endpoint transacao, add regra de negocio no atualizar Transacao e melhorando resposta dos endpoits transacoes)
 }
