78fc832062005c4029077e697df07df8fbd5fd25
Lucaasshq <lucashenriquebarros2016@gmail.com>
2026-05-06 01:47:02 -0300

    add validação do campo register

--- backend/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/web/controller/AuthController.java
+++ backend/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/web/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.npl.na_ponta_do_lapis.web.dto.auth.LoginRequestDTO;
 import com.npl.na_ponta_do_lapis.web.dto.auth.RegisterRequestDTO;
 import com.npl.na_ponta_do_lapis.web.dto.auth.TokenResponseDTO;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
@@ -30,7 +31,7 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody RegisterRequestDTO registerRequestDTO, HttpServletResponse response) {
+    public ResponseEntity<?> register(@RequestBody @Valid RegisterRequestDTO registerRequestDTO, HttpServletResponse response) {
         authService.signup(registerRequestDTO, response);
         return ResponseEntity.status(HttpStatus.CREATED).body("Usuário cadastrado com sucesso");
     }
