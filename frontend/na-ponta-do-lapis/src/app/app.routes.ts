7b59c4fca015a9db239543feb3c440a9353c5971
Lucaasshq <lucashenriquebarros2016@gmail.com>
2026-05-10 16:07:37 -0300

    feat: configurando rotas da pagina de login e register, add fotos do usuário via SQL

--- frontend/na-ponta-do-lapis/src/app/app.routes.ts
+++ frontend/na-ponta-do-lapis/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { TransacoesComponent } from './pages/transacoes/transacoes.component';
 import { ContasComponent } from './pages/contas/contas.component';
 import { authGuard } from './core/guard/auth.guard';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component'
+import { SignupComponent } from './auth/components/signup/signup.component';
 
 
 export const routes: Routes = [
     {path: "", redirectTo: "login", pathMatch:"full"},
     {path: "login", component: LoginComponent},
+    {path: 'signup', component:SignupComponent},
     {
         path: "app",
         component: MainLayoutComponent,
