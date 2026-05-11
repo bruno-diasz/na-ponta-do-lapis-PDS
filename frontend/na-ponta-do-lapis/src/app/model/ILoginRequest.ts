d3cec20d12d7c64849e123b44b411ed0ebae890d
Lucaasshq <lucashenriquebarros2016@gmail.com>
2026-05-10 22:25:28 -0300

    feat: refinando header

--- frontend/na-ponta-do-lapis/src/app/model/ILoginRequest.ts
+++ frontend/na-ponta-do-lapis/src/app/model/ILoginRequest.ts
@@ -2,3 +2,12 @@ export interface LoginRequest {
     email:string,
     password:string
 }
+
+export interface SignupRequest {
+    nomeCompleto: string,
+    email:string,
+    username:string
+    telefone:string,
+    password:string,
+    passwordConfirmation:string
+}
