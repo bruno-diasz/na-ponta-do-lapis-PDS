daa22fbcae7eac530e617ca85d25b52080091f12
Lucaasshq <lucashenriquebarros2016@gmail.com>
2026-05-10 22:23:48 -0300

    feat: add query para buscar transação por descrição, add atributo telefone

--- backend/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/repository/TransacaoRepository.java
+++ backend/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/repository/TransacaoRepository.java
@@ -8,6 +8,11 @@ import java.util.List;
 
 public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
 
-    @Query("SELECT t FROM Transacao t WHERE t.contaFinanceira.usuario.email = :email")
+    @Query("SELECT t FROM Transacao t WHERE t.contaFinanceira.usuario.email = :email ORDER BY t.dataHora desc ")
     List<Transacao> buscarTransacoesUsuarioLogado(String email);
+
+    @Query("SELECT  t FROM Transacao t WHERE t.contaFinanceira.usuario.email = :email " +
+            "AND LOWER(t.descricao) LIKE LOWER(CONCAT('%', :descricao, '%')) " +
+             "ORDER BY t.dataHora DESC")
+    List<Transacao> buscarPorDescricao(String email,String descricao);
 }
