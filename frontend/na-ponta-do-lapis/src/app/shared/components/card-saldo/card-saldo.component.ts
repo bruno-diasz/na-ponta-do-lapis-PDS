27bd27418ae062ec111d42b15abca803822cf374
Lucaasshq <lucashenriquebarros2016@gmail.com>
2026-05-29 09:49:31 -0300

    feat: add card de saldo em transações

---
+++ frontend/na-ponta-do-lapis/src/app/shared/components/card-saldo/card-saldo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { PrimeNGModuleModule } from '../../primeNg.module';
+
+@Component({
+  selector: 'app-card-saldo',
+  imports: [PrimeNGModuleModule],
+  templateUrl: './card-saldo.component.html',
+  styleUrl: './card-saldo.component.css',
+})
+export class CardSaldoComponent {}
