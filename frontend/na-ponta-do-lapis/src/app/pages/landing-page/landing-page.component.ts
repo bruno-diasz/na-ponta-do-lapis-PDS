54dd3b268b734259edd6156ce75fe2cad9825a5a
Erick <erickcarlos003@gmail.com>
2026-06-22 22:32:20 -0300

    Fix: Correção do menu flutuante

--- frontend/na-ponta-do-lapis/src/app/pages/landing-page/landing-page.component.ts
+++ frontend/na-ponta-do-lapis/src/app/pages/landing-page/landing-page.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [
     CommonModule,
-    HeroSectionComponent
+    HeroSectionComponent,
+    NavbarComponent
   ],
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css'],
