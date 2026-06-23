7fd79492f02b325f4b187d9501021da4619c69e5
Bruno Dias <brunoipb1@gmail.com>
2026-06-23 10:33:17 -0300

    Revert "Fix: Correção do menu flutuante"

--- frontend/na-ponta-do-lapis/src/app/pages/landing-page/landing-page.component.ts
+++ frontend/na-ponta-do-lapis/src/app/pages/landing-page/landing-page.component.ts
@@ -2,15 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeroSectionComponent } from './components/hero-section/hero-section.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [
     CommonModule,
-    HeroSectionComponent,
-    NavbarComponent
+    NavbarComponent,
+    HeroSectionComponent
   ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css',
