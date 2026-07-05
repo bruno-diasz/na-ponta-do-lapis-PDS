5e7c33e265f0695bbbda9c9fca7ab5e7720b1a76
Bruno Dias <brunoipb1@gmail.com>
2026-07-05 02:17:05 +0000

    fix: corrige env do front do dashboard

--- frontend/na-ponta-do-lapis/src/app/pages/dashboard/service/dashboard.service.ts
+++ frontend/na-ponta-do-lapis/src/app/pages/dashboard/service/dashboard.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../../../environments/environment.development';
+import { environment } from '@env';
 import { ICotacao } from '../../../model/ICotacao.models';
 
 @Injectable({ providedIn: 'root' })
